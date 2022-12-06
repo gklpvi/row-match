458df191d575be055364eee7c96331b7c214d26c
Gökalp Akartepe <g_akartepe@hotmail.com>
2022-12-06 11:59:16 +0300

    starting over

--- src/main/java/com/dreamgames/casestudy/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dreamgames.casestudy.repository;
-
-import com.dreamgames.casestudy.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-}
